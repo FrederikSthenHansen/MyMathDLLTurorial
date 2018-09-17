7f0c67198bddfd8d535b22fb9f69d93a6d7980be
AndersSkovNielsen <anderssnielsen@live.dk>
2018-09-17 10:50:12 +0200

    Added more tests

--- MyMathTester/MyMathTest.cs
+++ MyMathTester/MyMathTest.cs
@@ -18,16 +18,37 @@ namespace MyMathTester
         }
 
         [TestMethod]
-        public void addTestNegative()
+        public void ÁddTestNegative()
         {
             testMathResult = testMath.Add(-2, -4);
             Assert.IsTrue(testMathResult==-6);
         }
 
         [TestMethod]
-        public void addTestPositiveAndNegative()
+        public void AddTestPositiveAndNegative()
         {
             testMathResult = testMath.Add(5, -7);
+            Assert.IsTrue(testMathResult == -2);
+        }
+
+        [TestMethod]
+        public void AddTestThreeIntegers()
+        {
+            testMathResult = testMath.Add(8, 19, 6);
+            Assert.IsTrue(testMathResult == 33);
+        }
+
+        [TestMethod]
+        public void AddTestThreeLargeIntegers()
+        {
+            testMathResult = testMath.Add(10147, -1991, 2018);
+            Assert.IsTrue(testMathResult == 10174);
+        }
+
+        [TestMethod]
+        public void SubstractTest()
+        {
+            testMathResult = testMath.subract(7, 9);
             Assert.IsTrue(testMathResult == -2);
         }
     }
