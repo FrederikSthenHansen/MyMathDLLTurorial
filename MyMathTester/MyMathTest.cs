cebc0e6810bfb8204c7f54722ebcbbf6b18ed827
Frederik Sthen Hansen <fred305c@edu.easj.dk>
2018-09-17 10:06:22 +0200

    C sharp project added

---
+++ MyMathTester/MyMathTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMathDLLTurorial;
+
+namespace MyMathTester
+{
+    [TestClass]
+    public class MyMathTest
+    {
+        MathMachine testMath = new MathMachine();
+        private int testMathResult;
+        [TestMethod]
+        public void AddTest()
+        {
+            
+           testMathResult = testMath.Add(1, 2);
+            Assert.IsTrue(testMathResult==3);
+        }
+
+        [TestMethod]
+        public void addTestNegative()
+        {
+            testMathResult = testMath.Add(-2, -4);
+            Assert.IsTrue(testMathResult==-6);
+        }
+    }
+}
---
+++ MyMathTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyMathTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyMathTester")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("04d3e460-8df8-449c-9381-e7babf0b2066")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
